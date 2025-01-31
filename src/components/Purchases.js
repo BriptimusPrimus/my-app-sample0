79a4874157ae80ab7d6c8809c8f6b52d03837180
Roberto Brito <brituor@gmail.com>
2025-01-31 22:26:08 +0000

    Display data in table

--- src/components/Purchases.js
+++ src/components/Purchases.js
@@ -1,7 +1,35 @@
 
 function Purchases({ data }) {
     return (
-        <>{JSON.stringify(data)}</>
+        <>
+            <table>
+                <caption>Purchases</caption>
+                <thead>
+                    <tr>
+                        <th scope="col">Name</th>
+                        <th scope="col">Location</th>
+                        <th scope="col">Purchase Date</th>
+                        <th scope="col">Category</th>
+                        <th scope="col">Description</th>
+                        <th scope="col">Price</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {data.map((datum) => {
+                        return (
+                            <tr>
+                                <td>{datum.name}</td>
+                                <td>{datum.location}</td>
+                                <td>{datum.purchaseDate}</td>
+                                <td>{datum.category}</td>
+                                <td>{datum.description}</td>
+                                <td>{datum.price}</td>
+                            </tr>
+                        );
+                    })}
+                </tbody>
+            </table>
+        </>
     );
 };
 
